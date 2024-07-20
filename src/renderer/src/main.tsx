58d86a8c2a0178df7e1d7c94ab8089fea428d4db
wuyiccc <3406324191@qq.com>
2024-07-20 08:35:02 +0800

    feat: 安装tailwind css与配置

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,6 +3,7 @@ import './assets/main.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import '@renderer/assets/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
