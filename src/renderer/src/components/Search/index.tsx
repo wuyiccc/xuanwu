217988d76474e6f85e06d4b35e1869d20c5960e4
wuyiccc <3406324191@qq.com>
2024-07-21 22:02:33 +0800

    feat: 定义跳转到配置项的图标

--- src/renderer/src/components/Search/index.tsx
+++ src/renderer/src/components/Search/index.tsx
@@ -1,5 +1,6 @@
 import styles from './index.module.less'
 import useSearch from '../../hooks/useSearch'
+import { SettingOne } from '@icon-park/react'
 
 export default function () {
   const { search, handleSearch } = useSearch()
@@ -7,6 +8,14 @@ export default function () {
   return (
     <div className={styles.searchContainer}>
       <div className={styles.inputContainer}>
+        <SettingOne
+          theme="outline"
+          size="22"
+          fill="#34495e"
+          strokeWidth={4}
+          onClick={() => alert('显示配置页面')}
+          className={styles.setting}
+        />
         <input value={search} onChange={handleSearch} className={styles.inputText} />
       </div>
       <div className={styles.bottomTextLog}>xuanwu</div>
