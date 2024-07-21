00d237c00f478ad7de9a588e72e02c0d445c677f
wuyiccc <3406324191@qq.com>
2024-07-21 16:04:44 +0800

    feat: 快捷键可定制

--- src/main/code/shortCut.ts
+++ src/main/code/shortCut.ts
@@ -1,27 +1,27 @@
-import { app, BrowserWindow, dialog, globalShortcut } from 'electron'
+import { app, BrowserWindow, dialog, globalShortcut, ipcMain } from 'electron'
+import IpcMainEvent = Electron.IpcMainEvent
 
 export const registerShortCut = (win: BrowserWindow) => {
-  app.whenReady().then(() => {
+  ipcMain.on('shortCut', (_event: IpcMainEvent, shortCut: string) => {
     // 注册一个'CommandOrControl+X' 快捷键监听器
-    const ret = globalShortcut.register('CommandOrControl+Shift+;', () => {
+    const ret = globalShortcut.register(shortCut, () => {
       // console.log('CommandOrControl+X is pressed')
-      win.show()
+      if (win.isVisible()) {
+        win.hide()
+      } else {
+        win.show()
+      }
     })
-
     if (!ret) {
       dialog.showErrorBox('温馨提示', '快捷键注册失败')
-      // console.log('registration failed')
     }
-
-    // 检查快捷键是否注册成功
-    console.log(globalShortcut.isRegistered('CommandOrControl+X'))
-  })
-
-  app.on('will-quit', () => {
-    // 注销快捷键
-    // globalShortcut.unregister('CommandOrControl+X')
-
-    // 注销所有快捷键
-    globalShortcut.unregisterAll()
   })
 }
+
+app.on('will-quit', () => {
+  // 注销快捷键
+  // globalShortcut.unregister('CommandOrControl+X')
+
+  // 注销所有快捷键
+  globalShortcut.unregisterAll()
+})
