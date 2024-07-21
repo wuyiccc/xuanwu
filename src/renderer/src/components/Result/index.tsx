1480a24dc763c76282c69054c2928d7f36d08cc0
wuyiccc <3406324191@qq.com>
2024-07-21 08:12:11 +0800

    feat: 使用useCallback优化取值问题

--- src/renderer/src/components/Result/index.tsx
+++ src/renderer/src/components/Result/index.tsx
@@ -1,44 +1,48 @@
 import StatusDB from '../../status/StatusDB'
 import styles from './index.module.less'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import StringUtils from '../../utils/StringUtils'
 
 export default function () {
   const searchList = StatusDB.db((state) => state.searchList)
   const [currentIndex, setCurrentIndex] = useState(0)
 
-  const handleKeyDown = (e: KeyboardEvent) => {
-    if (searchList.length === 0) {
-      return
-    }
-    switch (e.code) {
-      case 'ArrowUp':
-        setCurrentIndex((currentIndex) =>
-          currentIndex - 1 < 0 ? searchList.length - 1 : currentIndex - 1
-        )
-        break
-      case 'ArrowDown':
-        setCurrentIndex((currentIndex) =>
-          currentIndex + 1 >= searchList.length ? 0 : currentIndex + 1
-        )
-        break
-      case 'Enter':
-        // 回车的时候复制文本
-        navigator.clipboard.writeText(searchList[currentIndex].content)
-    }
-  }
+  const handleKeyEvent = useCallback(
+    (e: KeyboardEvent) => {
+      if (searchList.length === 0) {
+        return
+      }
+      switch (e.code) {
+        case 'ArrowUp':
+          setCurrentIndex((currentIndex) =>
+            currentIndex - 1 < 0 ? searchList.length - 1 : currentIndex - 1
+          )
+          break
+        case 'ArrowDown':
+          setCurrentIndex((currentIndex) =>
+            currentIndex + 1 >= searchList.length ? 0 : currentIndex + 1
+          )
+          break
+        case 'Enter':
+          // 回车的时候复制文本
+          navigator.clipboard.writeText(searchList[currentIndex].content)
+      }
+      // 当searchList, currentIndex发生变更的时候重新生成函数
+    },
+    [searchList, currentIndex]
+  )
 
   useEffect(() => {
     setCurrentIndex(0)
   }, [searchList])
 
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown)
+    document.addEventListener('keydown', handleKeyEvent)
 
     return () => {
-      document.removeEventListener('keydown', handleKeyDown)
+      document.removeEventListener('keydown', handleKeyEvent)
     }
-  }, [searchList, currentIndex])
+  }, [handleKeyEvent])
 
   return (
     <div className={styles.itemContainer}>
