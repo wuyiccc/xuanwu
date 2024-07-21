49d5818e6e5ebbc821d0a3bb5d44a1f3b8d71d75
wuyiccc <3406324191@qq.com>
2024-07-21 08:42:28 +0800

    feat: 使用id控制代码段的选择

--- src/renderer/src/components/Result/index.tsx
+++ src/renderer/src/components/Result/index.tsx
@@ -3,14 +3,14 @@ import StringUtils from '../../utils/StringUtils'
 import useCodeSelect from '../../hooks/useCodeSelect'
 
 export default function () {
-  const { codeList, currentIndex } = useCodeSelect()
+  const { codeList, id } = useCodeSelect()
 
   return (
     <div className={styles.itemContainer}>
-      {codeList.map((item, index) => (
+      {codeList.map((item) => (
         <div
           key={item.id}
-          className={`${styles.item} ${currentIndex === index ? styles.itemSelected : StringUtils.EMPTY}`}
+          className={`${styles.item} ${id === item.id ? styles.itemSelected : StringUtils.EMPTY}`}
         >
           {item.content}
         </div>
--- src/renderer/src/hooks/useCodeSelect.tsx
+++ src/renderer/src/hooks/useCodeSelect.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useEffect, useState } from 'react'
 import StatusDB from '../status/StatusDB'
+import StringUtils from '../utils/StringUtils'
 
 export default () => {
   const codeList = StatusDB.db((state) => state.codeList)
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [id, setId] = useState(0)
   const handleKeyEvent = useCallback(
     (e: KeyboardEvent) => {
       if (codeList.length === 0) {
@@ -11,27 +12,30 @@ export default () => {
       }
       switch (e.code) {
         case 'ArrowUp':
-          setCurrentIndex((currentIndex) =>
-            currentIndex - 1 < 0 ? codeList.length - 1 : currentIndex - 1
-          )
+          // setId((currentIndex) => (currentIndex - 1 < 0 ? codeList.length - 1 : currentIndex - 1))
+          setId((id) => {
+            const index = codeList.findIndex((item) => item.id == id)
+            return codeList[index - 1]?.id || codeList[codeList.length - 1].id
+          })
           break
         case 'ArrowDown':
-          setCurrentIndex((currentIndex) =>
-            currentIndex + 1 >= codeList.length ? 0 : currentIndex + 1
-          )
+          // setId((currentIndex) => (currentIndex + 1 >= codeList.length ? 0 : currentIndex + 1))
+          setId((id) => {
+            const index = codeList.findIndex((item) => item.id == id)
+            return codeList[index + 1]?.id || codeList[0].id
+          })
           break
-        case 'Enter':
+        case 'Enter': {
+          const content = codeList.find((item) => item.id == id)?.content
           // 回车的时候复制文本
-          navigator.clipboard.writeText(codeList[currentIndex].content)
+          navigator.clipboard.writeText(content || StringUtils.EMPTY)
+          break
+        }
       }
       // 当searchList, currentIndex发生变更的时候重新生成函数
     },
-    [codeList, currentIndex]
+    [codeList, id]
   )
-
-  useEffect(() => {
-    setCurrentIndex(0)
-  }, [codeList])
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyEvent)
@@ -41,5 +45,5 @@ export default () => {
     }
   }, [handleKeyEvent])
 
-  return { codeList, currentIndex }
+  return { codeList, id }
 }
