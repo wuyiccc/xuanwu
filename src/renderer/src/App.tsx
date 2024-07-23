79a7bf7fbb1e9ed418f8dff2494c427e057e1e87
wuyiccc <3406324191@qq.com>
2024-07-23 22:20:19 +0800

    feat: 通过body实现鼠标穿透

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -7,13 +7,16 @@ import { useEffect, useRef } from 'react'
 function App(): JSX.Element {
   const mainRef = useRef<HTMLDivElement | null>(null)
   useEffect(() => {
-    mainRef.current?.addEventListener('mouseover', (e: MouseEvent) => {
-      console.log('mouseover')
+    mainRef.current?.addEventListener('mouseover', () => {
+      // 鼠标移入 则关闭穿透
       window.api.setIgnoreMouseEvents(false)
     })
-    mainRef.current?.addEventListener('mouseout', (e: MouseEvent) => {
-      console.log('mouseout')
-      window.api.setIgnoreMouseEvents(true, { forward: true })
+
+    document.body?.addEventListener('mouseover', (e: MouseEvent) => {
+      if (e.target === document.body) {
+        // 鼠标移入到body, 就加上穿透
+        window.api.setIgnoreMouseEvents(true, { forward: true })
+      }
     })
   }, [])
 
@@ -23,7 +26,7 @@ function App(): JSX.Element {
   window.api.setIgnoreMouseEvents(false, { forward: true })
 
   return (
-    <div ref={mainRef}>
+    <div ref={mainRef} style={{ padding: '1.25rem' }}>
       <Error />
       <Search />
       <Result />
