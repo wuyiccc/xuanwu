3a67a80f9372b9280c6bc1609297da9a9f4b381c
wuyiccc <3406324191@qq.com>
2024-07-24 22:07:01 +0800

    feat: 解决编译之后打开路由窗口失败的问题

--- src/main/config/window.ts
+++ src/main/config/window.ts
@@ -3,6 +3,7 @@ import { is } from '@electron-toolkit/utils'
 import icon from '../../../resources/icon.png?asset'
 // import { shell, BrowserWindow, screen } from 'electron'
 import { shell, BrowserWindow } from 'electron'
+import * as url from 'node:url'
 
 export function createWindow(): BrowserWindow {
   // const { width } = screen.getPrimaryDisplay().workAreaSize
@@ -42,9 +43,20 @@ export function createWindow(): BrowserWindow {
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'] + '/#config')
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadURL(
+      url.format({
+        // 编译后的文件
+        pathname: join(__dirname, '../renderer/index.html'),
+        // 协议
+        protocol: 'file',
+        // protocol后面需要两个/
+        slashes: true,
+        // hash的值
+        hash: 'config'
+      })
+    )
   }
 
   return mainWindow
