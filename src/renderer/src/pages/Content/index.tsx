1195d108909eb4dd0bc2fb90e611af3c95ee7172
wuyiccc <3406324191@qq.com>
2024-07-28 21:59:57 +0800

    feat: 片段编辑页面

--- src/renderer/src/pages/Content/index.tsx
+++ src/renderer/src/pages/Content/index.tsx
@@ -5,8 +5,10 @@ export default function () {
   const content = useLoaderData() as ContentEntity[]
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>{content[0].title}</h1>
-      <div className={styles.content}>{content[0].content}</div>
+      {/*<h1 className={styles.title}>{content[0].title}</h1>*/}
+      <input className={styles.title} defaultValue={content[0].title} />
+      {/*<div className={styles.content}>{content[0].content}</div>*/}
+      <textarea className={styles.content} defaultValue={content[0].content}></textarea>
     </div>
   )
 }
