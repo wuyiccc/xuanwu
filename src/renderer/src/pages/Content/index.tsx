3b9aed325ec9da1dbce65674ddc158728dbb1e7d
wuyiccc <3406324191@qq.com>
2024-07-31 21:09:12 +0800

    feat: 编辑页面样式优化

--- src/renderer/src/pages/Content/index.tsx
+++ src/renderer/src/pages/Content/index.tsx
@@ -1,14 +1,22 @@
 import { useLoaderData } from 'react-router-dom'
 import styles from './index.module.less'
+import { Button, Form } from 'antd'
 
 export default function () {
   const content = useLoaderData() as ContentEntity[]
   return (
-    <div className={styles.container}>
-      {/*<h1 className={styles.title}>{content[0].title}</h1>*/}
-      <input className={styles.title} defaultValue={content[0].title} />
-      {/*<div className={styles.content}>{content[0].content}</div>*/}
-      <textarea className={styles.content} defaultValue={content[0].content}></textarea>
-    </div>
+    <Form method="PUT">
+      <div className={styles.container}>
+        {/*<h1 className={styles.title}>{content[0].title}</h1>*/}
+        <input className={styles.title} defaultValue={content[0].title} />
+        {/*<div className={styles.content}>{content[0].content}</div>*/}
+        <textarea className={styles.content} defaultValue={content[0].content}></textarea>
+        <div className={styles.saveButton}>
+          <Button type="default" size="small">
+            保存
+          </Button>
+        </div>
+      </div>
+    </Form>
   )
 }
