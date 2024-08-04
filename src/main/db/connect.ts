44b1a5ee1690ecad6a7287c0d1218b64cfca3ca7
wuyiccc <3406324191@qq.com>
2024-08-04 13:15:40 +0800

    feat: 修改数据库路径

--- src/main/db/connect.ts
+++ src/main/db/connect.ts
@@ -1,7 +1,16 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 import Database from 'better-sqlite3'
+import { app } from 'electron'
+import { resolve } from 'path'
+import fs from 'node:fs'
+const homeDirectory = app.getPath('home')
+const databaseDirectory = resolve(homeDirectory, '.xuanwu')
+console.log(databaseDirectory)
+if (!fs.existsSync(databaseDirectory)) {
+  fs.mkdirSync(databaseDirectory, { recursive: true })
+}
 
-const sqlite = new Database('/Users/wuxingyu/work/tmp/xuanwu/xuanwu.db')
+const sqlite = new Database(app.getPath('home') + '/.xuanwu/xuanwu.db')
 const db = drizzle(sqlite, { logger: true })
 
 export { db }
--- src/main/db/init.ts
+++ src/main/db/init.ts
@@ -2,7 +2,8 @@ import Database, * as BetterSqlite3 from 'better-sqlite3'
 import { resolve } from 'path'
 import { app } from 'electron'
 
-const file = resolve(app.getPath('home'), 'work', 'tmp', 'xuanwu', 'xuanwu.db')
+
+const file = resolve(app.getPath('home'), '.xuanwu', 'xuanwu.db')
 const db: BetterSqlite3.Database = new Database(file, {})
 db.pragma('journal_mode = WAL')
 
